2705579b03d365a3e5940bdc7510e78500557e6e
Creuma-Kuzola <creumakuzola@gmail.com>
2023-08-12 02:08:49 +0100

    refactor: preparing new portfolio

--- VUE/proudProjects.js
+++
@@ -1,6 +0,0 @@
-function changeProjectsFilter(e){
-    app.actualProjectTypeFilter = e.target.value
-    document.querySelector('.number-of-projects').innerText = app.projects.filter(project => project.type == e.target.value || e.target.value == 'all').length + 'Projects'
-}
-
-//document.getElementById('project-type').addEventListener('change', changeProjectsFilter)
